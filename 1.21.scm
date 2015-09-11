46dbe8913c6ed68a7bb552d093d06399626747f6
蛇崩乃音 <shin@posteo.jp>
2015-09-11 23:13:03 +0200

    Exercise 1.21

---
+++ 1.21.scm
@@ -0,0 +1,24 @@
+; Exercise 1.21
+; Use the smallest-divisor procedure to find the smallest divisor
+; of each of the following numbers:
+; 199, 1999, 19999.
+;
+; See section 1.2.6 for smallest-divisor
+; ---------------------------------
+
+(define (smallest-divisor n)
+   (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+   (cond ((> (square test-divisor) n) n)
+         ((divides? test-divisor n) test-divisor)
+         (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+   (= (remainder b a) 0))
+
+(smallest-divisor 199)
+
+(smallest-divisor 1999)
+
+(smallest-divisor 19999)
