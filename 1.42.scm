15fcc4dd0ca7416ed98aff816f5a9fa65ff195ff
蛇崩乃音 <shin@posteo.jp>
2015-09-13 00:29:59 +0200

    Exercise 1.42

---
+++ 1.42.scm
@@ -0,0 +1,16 @@
+; Exercise 1.42
+; Let f and g be two one-argument functions. The composition f after g is defined
+; to be the function x mapsto f(g(x)). Define a procedure compose
+; that implements composition. For example, if inc is a procedure that adds 1 to its argument,
+; ((compose square inc) 6)
+; => 49
+; ---------------------------------
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (square x) (* x x))
+
+(define (inc x) (+ x 1))
+
+((compose square inc) 6)
